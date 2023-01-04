f7ebe9027a22f4fc3b74d646ea0f33ebd9b688ea
onisim_albarov <onisim93@gmail.com>
2023-01-04 23:02:50 +0400

    fixed lesson 9,10

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,2 +1,2 @@
-SELECT MAX(average) as max, sbj.id, sbj.name, sbj.description, sbj.grade FROM (SELECT AVG(mark) AS average, s.id, s.name, s.description, s.grade FROM mark INNER JOIN subject s on s.id = mark.subject_id GROUP BY s.id, s.name, s.description, s.grade) AS sbj group by sbj.id, sbj.name, sbj.description, sbj.grade ORDER BY max DESC LIMIT 1;
-SELECT SUM(payment.amount), student.id, student.name FROM student INNER JOIN payment ON student.id = payment.student_id GROUP BY student.id, student.name HAVING SUM(payment.amount) < (SELECT AVG(amount) AS average_amount FROM payment);
+SELECT s.id, s.name, s.description, s.grade FROM subject s JOIN mark m ON s.id = m.subject_id GROUP BY s.id HAVING AVG(m.mark) > (SELECT AVG(mark) FROM mark);
+SELECT student.id, student.name FROM student LEFT JOIN payment ON student.id = payment.student_id GROUP BY student.id HAVING AVG(payment.amount) < (SELECT AVG(amount) FROM payment) ORDER BY id;
