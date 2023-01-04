3961ed898c1e269fd5b911e4a6e4bb2bccec19af
onisim_albarov <onisim93@gmail.com>
2023-01-04 15:35:53 +0400

    fixed lessons 6,9

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT * FROM payment p INNER JOIN paymenttype t WHERE p.type_id = t.id AND t.name = 'MONTHLY';
 SELECT * FROM mark m INNER JOIN subject s where m.subject_id = s.id AND s.name = 'Art';
-SELECT DISTINCT * FROM student s INNER JOIN payment p, paymenttype t WHERE p.student_id = s.id AND p.type_id = t.id AND t.name = 'WEEKLY';
-SELECT DISTINCT * FROM student s INNER JOIN mark m, subject sbj WHERE m.student_id = s.id AND m.subject_id = sbj.id AND sbj.name = 'Math';
+SELECT DISTINCT(s.id), s.name FROM student s INNER JOIN payment ON payment.student_id = s.id INNER JOIN paymenttype ON payment.type_id = paymenttype.id WHERE paymenttype.name = 'WEEKLY';
+SELECT * FROM student s INNER JOIN mark m, subject sbj WHERE m.student_id = s.id AND m.subject_id = sbj.id AND sbj.name = 'Math';
